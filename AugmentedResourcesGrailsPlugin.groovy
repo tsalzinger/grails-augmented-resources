5c27d1f15d298d83c31bdab52baad0f93000292a
Thomas Scheinecker <tscheinecker@gmail.com>
2013-07-12 15:44:29 +0200

    prepare for development of 1.0.RC2

--- AugmentedResourcesGrailsPlugin.groovy
+++ AugmentedResourcesGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AugmentedResourcesGrailsPlugin {
     // the plugin version
-    def version = "1.0.RC1"
+    def version = "1.0.RC2-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
 	// the other plugins this plugin depends on
