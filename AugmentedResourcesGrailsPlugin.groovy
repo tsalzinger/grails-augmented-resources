8e8ffc57746a5569d22717b9da0b53aa930e543d
Burt Beckwith <burt@burtbeckwith.com>
2013-07-13 04:32:40 -0400

    don't use dependsOn in 2.0+ plugins, use dependency management in BuildConfig.groovy

--- AugmentedResourcesGrailsPlugin.groovy
+++ AugmentedResourcesGrailsPlugin.groovy
@@ -3,8 +3,6 @@ class AugmentedResourcesGrailsPlugin {
     def version = "1.0.RC1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
-	// the other plugins this plugin depends on
-	def dependsOn = [resources:'1.1.6 > *']
 	def loadAfter = ['resources']
 	// resources that are excluded from plugin packaging
 	def pluginExcludes = [
