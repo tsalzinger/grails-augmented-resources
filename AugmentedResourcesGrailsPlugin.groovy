2f2a05db5c09becd2e0c7a4b2b95758f40cf2ea8
Burt Beckwith <burt@burtbeckwith.com>
2013-07-13 04:32:08 -0400

    dropped minimum Grails version to 2.0

--- AugmentedResourcesGrailsPlugin.groovy
+++ AugmentedResourcesGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class AugmentedResourcesGrailsPlugin {
     // the plugin version
     def version = "1.0.RC1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.2 > *"
+    def grailsVersion = "2.0 > *"
 	// the other plugins this plugin depends on
 	def dependsOn = [resources:'1.1.6 > *']
 	def loadAfter = ['resources']
